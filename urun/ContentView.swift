9d3bfce042c419af286c42ad600ea9f36edccaa0
Dreamz2 <Edwei395@gmail.com>
2025-02-18 23:00:45 -0800

    Initial Commit

---
+++ urun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  urun
+//
+//  Created by Edison Wei on 2025-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ urun/urunApp.swift
@@ -0,0 +1,17 @@
+//
+//  urunApp.swift
+//  urun
+//
+//  Created by Edison Wei on 2025-02-18.
+//
+
+import SwiftUI
+
+@main
+struct urunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
